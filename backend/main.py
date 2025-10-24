979bb348f01af1edcc379cd2388505f35c6ebfd5
Leo <leoquan09@gmail.com>
2025-10-23 19:38:34 -0700

    hi

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,14 @@ from config import app, db
 from flask import jsonify, request
 from models import to_do
 
+#Retrive all existing to-do items
 @app.route('/todos', methods=['GET'])
 def get_todos():
     todos = to_do.query.all()
     json_todos = list(map(lambda x: x.to_json(), todos))
     return jsonify({'to_do': json_todos}), 200
 
+#create a new to-do item
 @app.route('/create_todo', methods=['POST'])
 def create_todo():
     title = request.json.get('title')
@@ -22,6 +24,21 @@ def create_todo():
         db.session.commit()
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+    return jsonify('Message: To-Do created successfully!'), 201
+
+#edit an existing to-do item
+@app.route('/update_todo/<int:id>')
+def update_todo(id, methods=['PATCH']):
+    todo = to_do.query.get(id)
+
+    if not todo:
+        return jsonify({'error': 'To-Do not found!'}), 404
+    
+    data = request.json
+    todo.title = data.get('title', todo.title)
+    todo.description = data.get('description', todo.description)
+    todo.completed = data.get('completed', todo.completed)
+    todo.due_date = data.get('due_date', todo.due_date)
 
 if __name__ == '__main__':
     with app.app_context():
